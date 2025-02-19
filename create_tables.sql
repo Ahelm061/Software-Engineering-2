be98dcffca1b113b3002afb3f4898b895eee193e
Ahelm061 <ahelm061@live.kutztown.edu>
2025-02-19 13:59:14 -0500

    Delete create_tables.sql

--- create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    answerOne TEXT NOT NULL,
-    answerTwo TEXT NOT NULL,
-    answerThree TEXT NOT NULL
-);
